4415f9ab5710a4355cabc113a6dbba50f2fa9c23
piaorongzhen <prz_2022@qq.com>
2023-05-10 23:00:03 +0800

    login

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,11 +1,53 @@
 <script setup>
+import { reactive } from 'vue'
+import { User, Lock } from '@element-plus/icons-vue'
 
+
+const form = reactive({
+    username: '',
+    password: '',
+})
+
+const onSubmit = () => {
+    console.log('submit!', form)
+}
 </script>
 
 <template>
-    <div>
-        login
-    </div>
+    <el-container class="login-container">
+        <el-main>
+            <el-form :model="form">
+                <el-form-item>
+                    <el-input v-model="form.username" :prefix-icon="User" clearable />
+                </el-form-item>
+                <el-form-item>
+                    <el-input v-model="form.password" :prefix-icon="Lock" clearable show-password />
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" @click="onSubmit">登录</el-button>
+                </el-form-item>
+            </el-form>
+        </el-main>
+    </el-container>
 </template>
 
-<style scoped></style>
+<style scoped>
+.login-container {
+    height: 100vh;
+    width: 100vw;
+}
+
+.el-main {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.el-input {
+    width: 300px;
+}
+
+.el-button {
+    width: 300px;
+}
+</style>
